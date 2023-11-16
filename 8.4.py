cfa5fd1cc9c1d6c54a9c6f8cba565f60d684313f
nguyenanh <148098785+nguyenanh130@users.noreply.github.com>
2023-11-16 21:42:11 +0700

    Update 8.4.py

--- 8.4.py
+++ 8.4.py
@@ -1,8 +1,11 @@
-var = 10
-while var > 0:
-    if var %2 ==0:
-          print("giá trị :", var)
-    var -=1
-    if var ==5:
-           break
-print("kết thúc vòng lặp!")    
+a = eval(input("nhap do dai canh a: "))
+b = eval(input("nhap do dai canh b: "))
+c = eval(input("nhap do dai canh c: "))
+if a+b<c and b+c<b:
+    print("day khong phai la tam giac")
+else:
+    print("day la mot tam giac")
+    import math
+    p=(a+b+c)/2
+    S= math.sqrt(p*(p-a)*(p-b)*(p-c))
+    print("Dien tich tam giac tren la: ",S)
